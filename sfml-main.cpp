a39866c16718d1b8b7a1283edd2aa48764540006
IsaacHorgan <theihh08@gmail.com>
2025-05-21 16:56:14 +0930

    text rendering

--- sfml-main.cpp
+++ sfml-main.cpp
@@ -1,22 +1,21 @@
 #include <SFML/Graphics.hpp>
-#include <optional>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Window");
+    
     while (window.isOpen())
     {
-        while (const std::optional event = window.pollEvent())
+        sf::Event event;
+        while (window.pollEvent(event))
         {
-            if (event->is<sf::Event::Closed>())
+            if (event.type == sf::Event::Closed)
                 window.close();
         }
 
-        window.clear();
-        window.draw(shape);
+        window.clear(sf::Color::Black);
         window.display();
     }
+
+    return 0;
 }
