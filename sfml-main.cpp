79a1e61e47500317398bdd254ced8ad99db7f83b
IsaacHorgan <theihh08@gmail.com>
2025-05-22 09:30:54 +0930

    detect clicking on card

--- sfml-main.cpp
+++ sfml-main.cpp
@@ -17,8 +17,12 @@ int main()
     title.setFont(font);
     title.setString("Lucky Card Game");
     title.setCharacterSize(36);
-    title.setFillColor(sf::Color::Green);
+    title.setFillColor(sf::Color::White);
     title.setPosition(200, 250);
+
+    sf::RectangleShape rect({100,150});
+    rect.setFillColor(sf::Color::Green);
+    rect.setPosition(500,300);
 
     while (window.isOpen()) {
         sf::Event event;
@@ -29,11 +33,18 @@ int main()
             if (event.type == sf::Event::MouseButtonPressed) {
                 std::string coords = std::to_string(event.mouseButton.x) + ", " + std::to_string(event.mouseButton.y);
                 title.setString(coords);
+                if ((500 <= event.mouseButton.x && event.mouseButton.x <= 600) && (300 <= event.mouseButton.y && event.mouseButton.y <= 450)) {
+                    title.setFillColor(sf::Color::Green);
+                }
+                else {
+                    title.setFillColor(sf::Color::White);
+                }
             }
         }
 
         window.clear(sf::Color::Black);
         window.draw(title);
+        window.draw(rect);
         window.display();
     }
 
